1f54ff738faa84fcd45def12af5a457f25cc0e26
pubudu-m <i.pubudumihiranga@gmail.com>
2025-03-27 19:36:28 +0530

    weather app

--- WeatherApp-FT/WeatherApp-FT/Services/NetworkService/Core/APIConstants.swift
+++ WeatherApp-FT/WeatherApp-FT/Services/NetworkService/Core/APIConstants.swift
@@ -9,5 +9,5 @@ import Foundation
 
 struct APIConstants {
     static let baseURLString = "api.openweathermap.org"
-    static let apiKey = "a07b185e617d1c9b831989a2d0703aef"
+    static let apiKey = ""
 }
